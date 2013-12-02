5782191753a2b07643b6415047c4edad1f53c63b
Arnold Benedict Tan Casis <atancasis@zvelo.com>
2013-12-01 19:31:15 -0800

    Modify function to return plain vector of labels.

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -16,16 +16,8 @@ negTerms <- c(afinn_list$word[afinn_list$score==-3 | afinn_list$score==-2 | afin
 posTerms <- c(afinn_list$word[afinn_list$score==3 | afinn_list$score==2 | afinn_list$score==1], "first-rate", "insightful", "clever", "charming", "comical", "charismatic", "enjoyable", "absorbing", "sensitive", "intriguing", "powerful", "pleasant", "surprising", "thought-provoking", "imaginative", "unpretentious")
 vPosTerms <- c(afinn_list$word[afinn_list$score==5 | afinn_list$score==4], "uproarious", "riveting", "fascinating", "dazzling", "legendary")
 
-#load up positive and negative sentences and format
-posText <- read.delim(file='polarityData/rt-polaritydata/rt-polarity-pos.txt', header=FALSE, stringsAsFactors=FALSE, fileEncoding="iso-8859-1")
-posText <- posText$V1
-posText <- unlist(lapply(posText, function(x) { str_split(x, "\n") }))
-negText <- read.delim(file='polarityData/rt-polaritydata/rt-polarity-neg.txt', header=FALSE, stringsAsFactors=FALSE, fileEncoding="iso-8859-1", quote="")
-negText <- negText$V1
-negText <- unlist(lapply(negText, function(x) { str_split(x, "\n") }))
-
 #function to calculate number of words in each category within a sentence
-sentimentScore <- function(sentence, vNegTerms, negTerms, posTerms, vPosTerms){
+sentimentScore <- function(sentence){
   #remove unnecessary characters and split up by word
   sentence <- gsub('[[:punct:]]', '', sentence)
   sentence <- gsub('[[:cntrl:]]', '', sentence)
@@ -46,24 +38,3 @@ sentimentScore <- function(sentence, vNegTerms, negTerms, posTerms, vPosTerms){
   score <- c(vNegMatches, negMatches, posMatches, vPosMatches)
   return(score)
 }
-
-#build tables of positive and negative sentences with scores
-posResult <- as.data.frame(sentimentScore(posText, vNegTerms, negTerms, posTerms, vPosTerms))
-negResult <- as.data.frame(sentimentScore(negText, vNegTerms, negTerms, posTerms, vPosTerms))
-posResult <- cbind(posResult, 'positive')
-colnames(posResult) <- c('sentence', 'vNeg', 'neg', 'pos', 'vPos', 'sentiment')
-negResult <- cbind(negResult, 'negative')
-colnames(negResult) <- c('sentence', 'vNeg', 'neg', 'pos', 'vPos', 'sentiment')
-
-#combine the positive and negative tables
-results <- rbind(posResult, negResult)
-
-#run the naive bayes algorithm using all four categories
-classifier <- naiveBayes(results[,2:5], results[,6])
-
-#display the confusion table for the classification ran on the same data
-confTable <- table(predict(classifier, results), results[,6], dnn=list('predicted','actual'))
-confTable
-
-#run a binomial test for confidence interval of results
-binom.test(confTable[1,1] + confTable[2,2], nrow(results), p=0.5)
